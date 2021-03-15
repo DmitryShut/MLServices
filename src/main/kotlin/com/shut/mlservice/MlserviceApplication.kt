f8704b135dcea7c655c0c16064bc6f8673d40295
Dmitry <dzsh0619@netcracker.com>
2021-03-15 13:46:41 +0300

    feat: initial commit

---
+++ src/main/kotlin/com/shut/mlservice/MlserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.shut.mlservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MlserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<MlserviceApplication>(*args)
+}
---
+++ src/test/kotlin/com/shut/mlservice/MlserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.shut.mlservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MlserviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
